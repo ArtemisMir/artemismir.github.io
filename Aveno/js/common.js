c4228ae57b75ec86b79321aac110a9fc852199ea
Artem Mironov <ss9888@ya.ru>
2022-01-26 12:21:41 +0300

    aveno

---
+++ Aveno/js/common.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	const swiper = new Swiper('.product_slider', {
+		
+		loop: false,
+		slidesPerView: "auto",
+		spaceBetween: 50,
+		grabCursor: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	
+	});
+
+	$('.menu-open-js').click(function () { 
+		$('.navbar').toggleClass('active')
+	});
+
+})
